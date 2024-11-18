254b26139d52bd8c4062d07f5c1a7d425a6b463d
Lisa-creates <77747128+Lisa-creates@users.noreply.github.com>
2024-11-18 23:42:06 +0300

    Update max_salary.sql

--- HW 3/Part 1/max_salary.sql
+++ HW 3/Part 1/max_salary.sql
@@ -1,5 +1,22 @@
------ with window functions -----
+----- without window functions -----
+WITH max_salary AS (
+    SELECT 
+       industry,
+       MAX(salary) AS max_sal
+    FROM salary 
+  	GROUP BY industry
+)
+SELECT 
+    first_name,
+    last_name,
+    salary,
+    s.industry,
+    first_name AS name_highest_sal
+FROM salary s
+INNER JOIN max_salary ms ON ms.max_sal = s.salary AND ms.industry = s.industry
+ORDER BY s.industry;
 
+----- with window functions -----
 WITH salary_employees AS (
     SELECT 
         first_name,
