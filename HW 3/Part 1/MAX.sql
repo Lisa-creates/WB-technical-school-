6b478268edcf603281ef5cdf494c78586788a99b
Lisa-creates <77747128+Lisa-creates@users.noreply.github.com>
2024-11-18 23:12:51 +0300

    Create MAX.sql

---
+++ HW 3/Part 1/MAX.sql
@@ -0,0 +1,21 @@
+----- with window functions -----
+
+WITH salary_employees AS (
+    SELECT 
+        first_name,
+        last_name,
+        salary,
+        industry,
+        FIRST_VALUE(first_name) OVER (PARTITION BY industry ORDER BY salary DESC) AS name_highest_sal,
+        MAX(salary) OVER (PARTITION BY industry) AS max_salary
+    FROM salary 
+)
+SELECT 
+    first_name,
+    last_name,
+    salary,
+    industry,
+    name_highest_sal
+FROM salary_employees 
+WHERE salary = max_salary 
+ORDER BY industry;
