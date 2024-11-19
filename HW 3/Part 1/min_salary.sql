ada2002b4ae17566bc854da042d79ddfec8832d1
Lisa-creates <77747128+Lisa-creates@users.noreply.github.com>
2024-11-19 12:59:16 +0300

    Update min_salary.sql

--- HW 3/Part 1/min_salary.sql
+++ HW 3/Part 1/min_salary.sql
@@ -1,4 +1,30 @@
         ----- Если в выводе нужно просто приписать рядом имя сотрудника, который получает самую большую зарплату в industry ------
+----- Without window functions -------
+WITH min_salary AS (
+    SELECT 
+        industry,
+        MIN(salary) AS min_sal
+    FROM salary 
+    GROUP BY industry
+), 
+lowest_paid_employee AS (
+    SELECT s.industry, 
+        first_name AS name_lowest_sal
+    FROM salary s 
+    INNER JOIN min_salary ms ON ms.min_sal = s.salary and ms.industry = s.industry
+)
+SELECT 
+    s.first_name,
+    s.last_name,
+    s.salary,
+    s.industry,
+    h.name_lowest_sal
+FROM 
+    salary s
+LEFT JOIN 
+    lowest_paid_employee h ON s.industry = h.industry
+ORDER BY 
+    s.industry; 
 
 ----- With window functions -------
 SELECT 
@@ -6,7 +32,7 @@ SELECT
     last_name,
     salary,
     industry,
-	FIRST_VALUE(first_name) OVER (PARTITION BY industry ORDER BY salary ASC) AS name_highest_sal
+	FIRST_VALUE(first_name) OVER (PARTITION BY industry ORDER BY salary ASC) AS name_lowest_sal 
 FROM salary
 ORDER BY industry; 
 
